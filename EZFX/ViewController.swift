3fe152798db5ab0ca344594cbd2f5e4402d8c600
Colin Hallett <colinhallett@gmail.com>
2019-10-14 15:46:26 +0100

    Initial Commit

---
+++ EZFX/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EZFX
+//
+//  Created by Colin on 14/10/2019.
+//  Copyright © 2019 ckzh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
