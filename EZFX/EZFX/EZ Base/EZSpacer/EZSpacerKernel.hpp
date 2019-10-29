2c68e052d3aea4610b3db3217e4aa7538fedb17d
Colin Hallett <colinhallett@gmail.com>
2019-10-29 12:16:01 +0000

    ez spacer empty

--- EZFX/EZFX/EZ Base/EZSpacer/EZSpacerKernel.hpp
+++ EZFX/EZFX/EZ Base/EZSpacer/EZSpacerKernel.hpp
@@ -31,30 +31,7 @@ public:
     void resetFX();
     
 public:
-    sp_revsc *reverb;
-    sp_crossfade *reverbCrossfadeL;
-    sp_crossfade *reverbCrossfadeR;
-    
-    AudioKitCore::AdjustableDelayLine leftDelayLine, rightDelayLine;
-    AudioKitCore::FunctionTableOscillator modOscillator;
-    float minDelayMs, maxDelayMs, midDelayMs, delayRangeMs;
-    float modFreqHz, modDepthFraction, dryWetMix;
-    sp_delay *delayL;
-    sp_delay *delayR;
-    sp_pshift *pshiftL;
-    sp_pshift *pshiftR;
-    sp_butlp *filterL;
-    sp_butlp *filterR;
-    AudioKitCore::AdjustableDelayLine leftFlangeLine, rightFlangeLine;
-    AudioKitCore::FunctionTableOscillator flangeOscillator;
-    float minFlangeMs, maxFlangeMs, midFlangeMs, flangeRangeMs;
-    float flangeModFreqHz, flangeModDepthFraction, flangeDryWetMix;
-    sp_phaser *phaser;
-    sp_crossfade *crossfadeL;
-    sp_crossfade *crossfadeR;
-    
-    float reverbTime = 0.0f;
-    float reverbStrength = 0.0f;
+
 };
 
 #endif /* __cplusplus */
